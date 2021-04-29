dbca7e5162803e6afe9742a5a2ace76d37f89204
moyi <78648778+moyi-qwq@users.noreply.github.com>
2021-04-29 14:59:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,56 +13,25 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 bro = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
-
 bro.get('https://e-report.neu.edu.cn/notes/create')
-
-#标签定位
 search_input = bro.find_element_by_id('un')
-#标签交互
 search_input.send_keys(USER)
-
-#标签定位
 search_input = bro.find_element_by_id('pd')
-#标签交互
 search_input.send_keys(PASS)
-
-#点击搜索按钮
 btn = bro.find_element_by_class_name('login_box_landing_btn')
 btn.click()
-
-sleep(1)
-
-if FLAG == "1":
-#点击搜索按钮
+if FLAG == '1':
     btn = bro.find_element_by_id('bind_email_later')
     btn.click()
-
-bro.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-#点击搜索按钮
 btn = bro.find_element_by_xpath('//*[@id="app"]/main/div/form/div[1]/table/tbody/tr/td[1]/div/div/div/label[1]/span[2]')
 btn.click()
-
-bro.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-#点击搜索按钮
 btn = bro.find_element_by_xpath('//*[@id="app"]/main/div/form/div[3]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span')
 btn.click()
 sleep(1)
-
-#点击搜索按钮
 btn = bro.find_element_by_xpath('//*[@id="app"]/main/div/form/div[4]/div[2]/table/tbody/tr[1]/td/div/div/div/label[1]/span[1]/span')
 btn.click()
-
-#点击搜索按钮
 btn = bro.find_element_by_xpath('//*[@id="app"]/main/div/form/div[6]/button/span')
 btn.click()
-
-
-
-
-
-
-
-
 
 
 
